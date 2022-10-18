0b993afbc95a260fd63f5986da5bea92f557e199
markschrik <56775030+markschrik@users.noreply.github.com>
2022-10-18 21:11:10 +0200

    test

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import {getAllProviders} from "rclone-api";
 
 getAllProviders().then(res => {
     console.log(res);
-    //... do something with the data
-}, error => {
-    //... do something with the error
-    //eg: alert(error)
+}).catch( error => {
     console.log(error);
 });
